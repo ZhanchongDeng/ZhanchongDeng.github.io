c86689ddc2114706d5f0d93b0a523b19bca1df80
ZhanchongDeng <47432662+ZhanchongDeng@users.noreply.github.com>
2020-06-19 02:23:44 -0700

    Updated more on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,90 @@
 import React from "react"
 
-import {Grid} from "@material-ui/core"
-
+import {Grid, Divider} from "@material-ui/core"
+import {Link} from 'gatsby'
 // For Background Image
 import '../css/index.css'
+import face from '../images/bob.jpg'
+
+const category = ['Name', 'University', 'Major', 'Expected Graduation date']
+const info = ['Zhanchong Deng', 'University of Calfornia, San Diego', 'Computer Science', '06/2022']
+const infoElement = []
+
+const linkStyle = {
+  color: '#e8a87c',
+  textDecoration: 'none',
+}
 
 const IndexPage = () => (
   <div className="root">
-  <Grid container
-    spacing={0}
-  >
-    <Grid item
-      xs={12}
-      className="selfie"
+    <Grid container
+      spacing={0}
     >
-        <div className="black-overlay">
-          <div className="welcome-message">
-            <p>Hey, I'm</p>
-            <h1>
-              &mdash;Bobby Deng&mdash;
-            </h1>
+      <Grid item
+        xs={12}
+        className="selfie"
+      >
+          <div className="black-overlay">
+            <div className="welcome-message">
+              <p>Hey, I'm</p>
+              <h1>
+                &mdash;Bobby Deng&mdash;
+              </h1>
+            </div>
           </div>
+      </Grid>
+      
+      <Grid item
+        xs={12}
+        className="section_title"
+      >
+        <h2>Who am I?</h2>
+      </Grid>
+
+      <Grid item
+        xs={12}
+        className="face"
+        align="center"
+      >
+        <div className="circular-crop">
+          <img className="face" src={face} alt='face of bob' />
         </div>
+      </Grid>
+      <Grid item
+        xs={12}
+        className="introduction"
+        align="center"
+      >
+        <div className="text">
+        <p>I am a <strong>full-stack developer</strong> and a <strong>data analyst with various</strong> <Link to = '404' style={linkStyle}>project/research experience.</Link></p>
+        <p> It is my passion to create applications/data analyzations to inspire others.</p>
+        </div>
+      </Grid>
     </Grid>
-
-    <Grid item
-      xs={12}
-      md={6}
+    
+    <Grid container
+      spacing={2}
     >
-      <p>Coming soon...</p>
+      
+      <Grid item
+        xs={12}
+        md={6}
+        className="preview_card"
+      >
+        <h2>What I have done</h2>
+        <p>Coming Soon...</p>
+      </Grid>
+
+      <Grid item
+        xs={12}
+        md={6}
+        className="preview_card"
+      >
+        <h2>What I can do</h2>
+        <p>Coming Soon...</p>
+      </Grid>
     </Grid>
 
-    <Grid item
-      xs={12}
-      md={6}
-    >
-      <p>Coming soon...</p>
-    </Grid>
-
-  </Grid>
   </div>
 )
 
